18d7e613f912cfd7323ed7c84c272622b6ddd30a
Leon Dong <leon.d1043@gmail.com>
2018-12-24 14:29:09 -0500

    shortened code

--- Buka.py
+++ Buka.py
@@ -1,6 +1,1 @@
-import operator
-ops = { "+": operator.add, "*": operator.mul }
-val = []
-for i in range(3):
-    val.append(input())
-print(ops[val[1]](int(val[0]), int(val[2])))
+print(eval(input() + input() + input()))
