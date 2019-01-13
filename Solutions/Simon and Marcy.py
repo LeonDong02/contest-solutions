0b278fdf36de8e80e83e14750ef25c08f339b64a
Leon Dong <leon.d1043@gmail.com>
2019-01-12 22:23:37 -0500

    my first dp problem!

---
+++ Solutions/Simon and Marcy.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+c, m = map(int, input().split())
+cages = [list(map(int, input().split())) for i in range(c)]
+cost = [0]*(m + 1)
+for i in range(1, c + 1):
+    for j in reversed(range(cages[i - 1][1], m + 1)):
+        cost[j] = max(cost[j], cages[i - 1][0] + cost[j - cages[i - 1][1]])
+print(cost[m])
