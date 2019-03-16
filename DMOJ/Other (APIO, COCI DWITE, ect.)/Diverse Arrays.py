c56ad0959230043d7f3eebca900c9dd656bafde9
Leon Dong <leon.d1043@gmail.com>
2019-03-16 16:23:32 -0400

    diverse arrays

---
+++ DMOJ/Other (APIO, COCI DWITE, ect.)/Diverse Arrays.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+
+n, k = map(int, input().split())
+f = [int(input()) for _ in range(n)]
+freq = {}
+l, c = 0, 0
+for r in range(n):
+    if f[r] not in freq:
+        freq[f[r]] = 0
+    freq[f[r]] += 1
+    while len(freq) >= k:
+        l += 1
+        freq[f[l - 1]] -= 1
+        if not freq[f[l - 1]]:
+            del freq[f[l - 1]]
+    c += l
+print(c)
