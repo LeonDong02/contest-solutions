ef1690f7570b1df291ea6d02ce16d56510b34047
Leon Dong <leon.d1043@gmail.com>
2020-01-14 21:15:53 -0500

    looks better

--- DMOJ/CCC/Circle of Life.py
+++ DMOJ/CCC/Circle of Life.py
@@ -9,14 +9,8 @@ n, t = map(int, input().split())
 s = list(map(int, list(input())))
 adv = 1
 while t:
-    cur = t & 1
-    t >>= 1
-    if cur:
-        temp = [0] * n
-        for i in range(n):
-            temp[i] = s[(i - adv) % n] ^ s[(i + adv) % n]
-        for i in range(n):
-            s[i] = temp[i]
-    adv <<= 1
-for i in s:
-    print(i, end='')
+    if t & 1:
+        temp = [s[(i - adv) % n] ^ s[(i + adv) % n] for i in range(n)]
+        s = [temp[i] for i in range(n)]
+    adv <<= 1; t >>= 1
+print(''.join(list(map(str, s))))
